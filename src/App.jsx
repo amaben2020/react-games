c5b4179091ef714f0e3b8806017ff2bb69e470e4
algomachine007 <amaben202phantom@gmail.com>
2024-01-14 17:59:04 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { questions } from "./data/data";
+
+// display the questions
+// select one option
+// display green border for correct and red for wrong
+// move to next question
+// ensure each question has 3 attempts
+// move to finish page with score when attempts are completed
+// give time constraint with timer i.e 2mins per question
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
